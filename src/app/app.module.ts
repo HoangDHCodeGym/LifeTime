5dae8a4b7e7898d910d7a177c6e2416d16962c3e
HoangDHCodeGym <ol.hoangdh.thna@gmail.com>
2018-11-27 11:45:18 +0700

    Update Youtube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import {AppRoutingModule} from './app-routing.module';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import {AppRountingYoutubeModule} from './app-rounting-youtube.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelinesComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppRountingYoutubeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
